fd4c9395c2bfb8ab51ace41ce72511b6424f2bbe
calvin <ccnlui@gmail.com>
2022-05-19 17:28:32 -0400

    description

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,5 +5,5 @@ import "github.com/spf13/cobra"
 // Cmd is the ekko client command
 var Cmd = &cobra.Command{
 	Use:   "client",
-	Short: "client that sends messages to server, and measure RTT latency in microseconds",
+	Short: "Send messages to server, and measure RTT latency in microseconds",
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,5 +5,5 @@ import "github.com/spf13/cobra"
 // Cmd is the ekko client command
 var Cmd = &cobra.Command{
 	Use:   "server",
-	Short: "server that echoes back every message",
+	Short: "Echo back every message to client",
 }
